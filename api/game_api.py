0e19f292ebf0550b04963210c0ca8c75f7ade76a
Daniel Holliday <danielholliday4@gmail.com>
2020-03-07 15:58:38 -0800

    room code

--- api/game_api.py
+++ api/game_api.py
@@ -1,8 +1,12 @@
-import time
+import random
+import string
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
+@app.route('/room')
+def get_room_code():
+    code_len = 4
+    letters = string.ascii_lowercase
+    code = ''.join(random.choice(letters) for i in range(code_len))
+    return {'room_code': code}
