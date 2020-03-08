bf0179e1b974dc7873065fc43d03896de888d201
Daniel Holliday <danielholliday4@gmail.com>
2020-03-07 16:04:07 -0800

    room code

--- client/game-of-things-app/src/App.js
+++ client/game-of-things-app/src/App.js
@@ -1,23 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [ roomCode, setRoomCode ] = useState(0);
+
+  useEffect(() => {
+    fetch('/room').then(res => res.json()).then(data => {
+      setRoomCode(data.room_code);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <p>Here is your room code {roomCode}</p>
       </header>
     </div>
   );
